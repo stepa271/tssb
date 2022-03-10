4e63a103250ed52d914bb8c8c2930921adaa246a
Vladimir S <9949498@gmail.com>
2022-03-10 13:32:56 +0300

    Стартовая конфигурация приложения MVC Урок 22. Методы для добавления и вывода в представление

--- src/main/java/com/tssb/controllers/PeopleController.java
+++ src/main/java/com/tssb/controllers/PeopleController.java
@@ -2,34 +2,54 @@ package com.tssb.controllers;
 
 
 import com.tssb.dao.PersonDAO;
+import com.tssb.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
     private PersonDAO personDAO;
+
     @Autowired
     public PeopleController(PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 
     @GetMapping()
-    public String index(Model model){
+    public String index(Model model) {
         //Получаем всех людей из DAO,передадим на отображение в представление
-        model.addAttribute("people",personDAO.index());
-        return  "people/index";
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+
+        /*person.setName(name);
+        person.setSurname(surname);
+        person.setEmail(email);*/
+
+        // добавляем в БД
+        personDAO.save(person);
+                return "redirect:/people";
     }
 
 
--- src/main/java/com/tssb/models/Person.java
+++ src/main/java/com/tssb/models/Person.java
@@ -3,10 +3,35 @@ package com.tssb.models;
 public class Person {
     private int id;
     private String name;
+    private String surname;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+    public Person() {}
 
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+    public Person(String name, String surname, String email){
+        this.name=name;
+        this.surname=surname;
+        this.email=email;
     }
 
     public int getId() {
